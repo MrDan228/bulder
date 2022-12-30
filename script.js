0ccf62463bf41751798cc7492e1102741f8819d2
MrDan228 <88660533+MrDan228@users.noreply.github.com>
2022-12-30 22:16:03 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.querySelector(".send-input").onclick = f1; 
+document.querySelector(".send-input").onclick = f2; 
+
+function f1(){
+console.log(document.querySelector(".phone-input").value)
+}
+function f2(){
+console.log(document.querySelector(".name-input").value)
+}
